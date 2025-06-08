80bd09d303bf6ce2c2e1e53fc77f1aaabcb7a0a8
jupa-aguilar <jaguilarviera@gmail.com>
2025-06-08 18:42:45 -0300

    Fix Vite config and script tag for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [
-		
-	]
+  base: './', // 👈 this is the important part for Netlify!
 })
