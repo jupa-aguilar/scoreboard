caaa011716045f450b09a9abf3293b6f1b133318
jupa-aguilar <jaguilarviera@gmail.com>
2025-06-08 18:19:54 -0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+let home = 0
+let guest = 0
+let homeCount = document.getElementById("home-counter")
+let guestCount = document.getElementById("guest-counter")
+homeCount.textContent = home
+guestCount.textContent = guest
+// Add Home functions
+function addOneHome() {
+    home += 1
+    updateHomeCounter()
+}
+function addTwoHome() {
+    home += 2
+    updateHomeCounter()
+}
+function addThreeHome() {
+    home += 3
+    updateHomeCounter()
+}
+function updateHomeCounter() {
+    homeCount.textContent = home
+}
+// Add Guest functions
+function addOneGuest() {
+    guest += 1
+    updateGuestCounter()
+}
+function addTwoGuest() {
+    guest += 2
+    updateGuestCounter()
+}
+function addThreeGuest() {
+    guest += 3
+    updateGuestCounter()
+}
+function updateGuestCounter() {
+    guestCount.textContent = guest
+}
